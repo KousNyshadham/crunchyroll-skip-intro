e68cfc0b2b165c52230fbae91ef81a7ba92b95d0
Kous <koustubh.nyshadham@gmail.com>
2020-05-05 23:11:31 -0500

    done before pretty

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var menu;
 async function timestamp(){
     bruh = window.location.toString();
     bro = await fetch(bruh).then(result =>result.text()).then(text=>text);
@@ -33,4 +34,21 @@ function bruh(){
 }
 playerloaded();
 isReady();
-bruh();
+menu = document.getElementsByClassName("showmedia-submenu white-wrapper cf container-shadow small-margin-bottom")[0];
+let twitter = document.getElementsByClassName("twitter")[0];
+twitter.style.marginRight = "12px";
+let span = document.createElement("span");
+span.classList.add("left");
+span.classList.add("facebook")
+let button = document.createElement("button");
+button.textContent = "Skip Intro";
+button.style.height = "20px";
+button.style.color = "white";
+button.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+button.style.border = "1px solid white";
+button.style.padding = "2px 5px 15px";
+button.style.borderRadius = "3px";
+button.style.cursor = "pointer";
+button.addEventListener("click", bruh);
+span.appendChild(button);
+menu.appendChild(span);
